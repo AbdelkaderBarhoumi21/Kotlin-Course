fbfec024c8f9f2b3da7c507a2cd56ee2505d78bc
Abdelkader <a.barhoumi@oxton-digital.com>
2026-05-07 17:27:51 +0100

    feat: add fucntions with named params and default values exampels

--- src/Functions/functions_examples.kt
+++ src/Functions/functions_examples.kt
@@ -13,10 +13,25 @@ fun display(mesage: String): Unit {
     println(mesage)
 }
 
+// functions with named params - Default values
+// Default values — avoids method overloading
+
+fun greet(name: String, greeting: String = "Good morning", punctuation: String = "!"): String =
+    "$greeting, $name$punctuation"
+
 fun main(args: Array<String>) {
     val result: Int = add(1, 2, 3)
     val result2 = add(1, 2)
     val message = display("Hello World!")
+    val greetingResult = greet("Abdelkader")
+    val greetingResult1 = greet("Abdelkader", "Good night")
+    val greetingResult2 = greet("Abdelkader", "Good night", "?")
+    val greetingResult3 = greet(punctuation = "?", name = "Abdelkader") // ordering doesnt matter
+
+    println(greetingResult)
+    println(greetingResult1)
+    println(greetingResult2)
+    println(greetingResult3)
     println(result)
     println(result2)
 
